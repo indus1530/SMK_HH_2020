bef1d775a25072e16f1701cbee05832ce27e5981
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-07-01 15:58:41 +0500

    fixed string eng and urdu for sectionUN

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_smk_hh/ui/sections/SectionUN.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_smk_hh.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.uen_smk_hh.R;
+
+public class SectionUN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_un);
+    }
+}
